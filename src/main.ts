e1ed5d893726d7473459b700d5b4580cd6640fe0
Valmaster <valentin.petit.95@hotmail.fr>
2025-01-21 06:51:29 +0100

    fix modules

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+import { Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const logger = new Logger('DataIDraw API');
+  const config = new DocumentBuilder()
+    .setTitle('DataIDraw API')
+    .setVersion('0.0.1')
+    .setDescription(
+      'DataIDraw Website',
+    )
+    .addTag('dataidraw-api')
+    .addBearerAuth()
+    .build();
+
+  app.use(cookieParser());
+  app.enableCors({
+    origin: ['*'],
+    credentials: true
+  });
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(3000);
+  logger.log(`Server started @http://locahost:3000`);
 }
+
 bootstrap();
