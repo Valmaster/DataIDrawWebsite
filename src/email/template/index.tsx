a57b0cb4992b6c77939e2de0a5da78fd48051ce4
Valmaster <valentin.petit.95@hotmail.fr>
2025-01-22 08:20:53 +0100

    test email template

--- src/email/template/index.tsx
+++ src/email/template/index.tsx
@@ -1,6 +1,5 @@
 import { Button, Html } from '@react-email/components';
 import * as React from 'react';
-import 'index.css';
 
 export default function Email({ url }) {
   return (
@@ -9,8 +8,11 @@ export default function Email({ url }) {
         href={url}
         style={{ background: '#000', color: '#fff', padding: '12px 20px' }}
       >
-        <div className={'container'}>
-          <h1>Test</h1>
+        <div
+          className={'container'}
+          style={{ background: 'red', width: '500px', height: '500px' }}
+        >
+          <h1 style={{ fontSize: '48px' }}>Test</h1>
         </div>
         Click me
       </Button>
